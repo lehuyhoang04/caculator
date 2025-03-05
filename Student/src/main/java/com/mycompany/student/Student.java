ea09b3df2c491851bafff78890e847518b99b154
lehuyhoang04 <hoangdepzai462004@gmail.com>
2025-03-05 18:46:36 +0700

    Add files via upload

---
+++ Student/src/main/java/com/mycompany/student/Student.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student;
+
+/**
+ *
+ * @author MSI
+ */
+public class Student {
+    private String studentId;
+    private String name;
+    private int age;
+
+    public Student(String studentId, String name, int age) {
+        this.studentId = studentId;
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getStudentId() { return studentId; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    public void updateName(String newName) { this.name = newName; }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
